df6dc39772db22fb52e53d8e04b3313b6dea0557
Callum Spiers <20201868@student.glasgowclyde.ac.uk>
2019-06-06 15:56:00 +0100

    Date: 06/06/19

--- js/onScroll.js
+++ js/onScroll.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
 	  if (scroll > 900) {
 	    $("nav").css("background" , "#fff");
 		$("nav").css("border-bottom" , "3px solid #000");
+		$(".icon i").css("color", "#000");
 	  }
 
 	  else{
 		  $("nav").css("background" , "transparent");
 		  $("nav").css("border" , "none");
+		  $(".icon i").css("color", "#fff");
 	  }
   })
 })
---
+++ js/responsiveNav.js
@@ -0,0 +1,3 @@
+$('.icon').on('click', function(){
+    $('.list').toggle();
+});
